669987f0ebd01963d0eb52849fd4f16640350232
Justin Worthe <justin@worthe-it.co.za>
2017-10-16 21:57:54 +0200

    Added correlation function which updated array in place

--- src/emscripten_api.rs
+++ src/emscripten_api.rs
@@ -2,10 +2,10 @@ use transforms;
 
 use std::os::raw::c_char;
 use std::ffi::CString;
+use std::slice;
 
 #[no_mangle]
 pub extern "C" fn find_fundamental_frequency(signal: *const f32, signal_length: isize, sample_rate: f32) -> f32 {
-    use std::slice;
     let signal_slice = unsafe {
         &slice::from_raw_parts(signal, signal_length as usize)
     };
@@ -24,4 +24,19 @@ pub extern "C" fn hz_to_pitch(hz: f32) -> *mut c_char {
     CString::new(pitch)
 		.unwrap()
         .into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn correlation(signal: *mut f32, signal_length: isize) {
+    let signal_slice = unsafe {
+        &slice::from_raw_parts(signal, signal_length as usize)
+    };
+
+    let correlated_signal = transforms::correlation(&signal_slice);
+
+    unsafe {
+        for (i, cor) in correlated_signal.iter().enumerate() {
+            *signal.offset(i as isize) = *cor;
+        }
+    }
 }
--- web/main.js
+++ web/main.js
@@ -13,8 +13,26 @@ function jsArrayToF32ArrayPtr(jsArray, callback) {
     var dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
     dataHeap.set(new Uint8Array(data.buffer));
 
-    var result = callback(dataHeap.byteOffset, jsArray.length);
+    var result = callback(dataPtr, jsArray.length);
 
+    Module._free(dataPtr);
+    
+    return result;
+}
+
+function jsArrayToF32ArrayPtrMutateInPlace(jsArray, mutate) {
+    var data = new Float32Array(jsArray);
+    var nDataBytes = data.length * data.BYTES_PER_ELEMENT;
+    var dataPtr = Module._malloc(nDataBytes);
+
+    var dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+    dataHeap.set(new Uint8Array(data.buffer));
+
+    mutate(dataPtr, jsArray.length);
+
+    var mutatedData = new Float32Array(Module.HEAPU8.buffer, dataPtr, jsArray.length);
+    var result = Array.prototype.slice.call(mutatedData);
+    
     Module._free(dataPtr);
     
     return result;
@@ -35,13 +53,21 @@ function hzToPitch(hz) {
     return wrapped(hz);
 }
 
+function correlation(data) {
+    return jsArrayToF32ArrayPtrMutateInPlace(data, function(dataPtr, dataLength) {
+        Module._correlation(dataPtr, dataLength);
+    });
+}
+
 function main() {
     var data = [1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0];
     var fundamental = findFundamentalFrequency(data, 44100.0);
+    var correlated = correlation(data);
 
     var error = hzToCentsError(450.0);
     var pitch = hzToPitch(450.0);
     
     console.log("Javascript here. Our fundamental frequency according to Rust is " + fundamental + "Hz");
     console.log("The other math shows a pitch of " + pitch + ", and an error of " + error);
+    console.log("Correlation of the array is " + correlated);
 }
