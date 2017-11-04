ed87b9354ac737185eb177bb62e4c3f579cd52b7
Justin Worthe <justin@worthe-it.co.za>
2017-11-04 16:55:29 +0200

    Changed wasm build to use release mode
    The performance difference was significant.

--- make-web.sh
+++ make-web.sh
@@ -2,10 +2,10 @@
 
 DIR="$(dirname "$0")"
 
-if cargo build --target=wasm32-unknown-emscripten; then
-    cp $DIR/target/wasm32-unknown-emscripten/debug/rusty_microphone.js "$DIR/web/"
-    cp $DIR/target/wasm32-unknown-emscripten/debug/deps/*.wasm "$DIR/web/"
-    cp $DIR/target/wasm32-unknown-emscripten/debug/deps/*.wast "$DIR/web/"
-    cp $DIR/target/wasm32-unknown-emscripten/debug/deps/*.map "$DIR/web/"
-    cp $DIR/target/wasm32-unknown-emscripten/debug/deps/*.js "$DIR/web/"
+if cargo build --target=wasm32-unknown-emscripten --release; then
+    cp $DIR/target/wasm32-unknown-emscripten/release/rusty_microphone.js "$DIR/web/"
+    cp $DIR/target/wasm32-unknown-emscripten/release/deps/*.wasm "$DIR/web/"
+    cp $DIR/target/wasm32-unknown-emscripten/release/deps/*.wast "$DIR/web/"
+    cp $DIR/target/wasm32-unknown-emscripten/release/deps/*.map "$DIR/web/"
+    cp $DIR/target/wasm32-unknown-emscripten/release/deps/*.js "$DIR/web/"
 fi
